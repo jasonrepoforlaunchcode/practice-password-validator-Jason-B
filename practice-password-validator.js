f1ea857c4d6022064e32b5d8fef2a3632e4c8d73
jasonrepoforlaunchcode <jasonrepoforlaunchcode@gmail.com>
2025-03-10 13:23:23 -0500

    Read assignment more closely and corrected.

--- practice-password-validator.js
+++ practice-password-validator.js
@@ -1,14 +1,50 @@
 const input = require('readline-sync');
-let userPassword = 'Armadilla';
-let response = 'Cactus';
-console.log(`(The password....is ${userPassword})\n`);
+
+let password = "";
+let passwordPassesCriteria = false;
+console.log("Passwords must be 8 characters long and include 1 capital letter and 1 number.")
 do {
-    response = input.question("\nPlease enter your password: ");
-    if (response !== userPassword){
-        console.log("Try again. (Remember partner... passwords are case sensitive!)");
-        if (response.toLowerCase() === "armadillo"){
-            console.log("(Can't you spell!?)");
+    password = "";
+    passwordPassesCriteria = false;
+    let passwordMeetsLengthRequirement = false;
+
+    password = input.question("Please enter your password: ");
+
+    if (password.length >= 8){
+        passwordMeetsLengthRequirement = true;
+    }
+
+    let capitalLetterFound = false;
+    let numberFound = false;
+    for(letter of password){
+        let letterIsANumber = false;
+        for(i = 0; i < 10; i++){
+            if (letter.toString() === i.toString()){
+                numberFound = true;
+                letterIsANumber = true;
+            }
+        }
+        if (letter === letter.toUpperCase() && letterIsANumber === false){
+            capitalLetterFound = true;
+        }
+
+    }
+    if(passwordMeetsLengthRequirement && capitalLetterFound && numberFound){
+        passwordPassesCriteria = true;
+    }
+
+    if (!passwordPassesCriteria){
+        console.log("")
+        if (password.length < 8){
+            console.log("Passwords must be at least 8 characters long");
+        }
+        if (!numberFound){
+            console.log("Passwords must contain at least 1 number.")
+        }
+        if (!capitalLetterFound){
+            console.log("Passwords must contain at least 1 capital letter.")
         }
     }
-} while(response !== userPassword);
-console.log("Welcome to the Autry Museum of the American West!");
+} while(!passwordPassesCriteria)
+
+console.log("Password accepted.");
